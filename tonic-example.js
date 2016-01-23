a0e8a55e1b27213e19440819969c702bca83771b
Matanya Fishaimer <matfish2@gmail.com>
2016-01-23 10:34:56 +0200

    remove tonic example

--- tonic-example.js
+++
@@ -1,7 +0,0 @@
-// Well, You can't present a form without a document, but as a POC the object should have an `installed:true` property, indicating we are good to go
-
-var Vue = require("vue")
-var VueFormular = require("vue-formular")
-
-Vue.use(VueFormular)
-VueFormular
